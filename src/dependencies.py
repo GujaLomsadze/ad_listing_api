83a2f12bff7607ac9c533759ea6c2a0794a3ffe1
Lomsadze <elguja.lomsadze.1@btu.edu.ge>
2023-03-07 19:23:52 +0400

    AA-02 Importing and base endpoints, base main.py

---
+++ src/dependencies.py
@@ -0,0 +1,37 @@
+from fastapi import Header, HTTPException
+
+from .database import SessionLocal
+
+from jose import jwt
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+ALGORITHM = "HS256"
+
+
+def decode(token):
+    striped_token = token.replace("Bearer ", "")
+    payload = jwt.decode(striped_token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    return payload
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+async def get_token_header(x_token: str = Header(...)):
+    payload = decode(x_token)
+    username: str = payload.get("email")
+
+    if username is None:
+        raise HTTPException(status_code=403, detail="Unauthorized")
+
+
+async def get_query_token(token: str):
+    if token != "jessica":
+        raise HTTPException(status_code=400, detail="No Valid token provided")
